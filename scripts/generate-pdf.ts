7909ea1715c84904a575a0167d6ab02644aa50b1
Owen B <owen@owenberry.com>
2017-11-07 00:45:25 +1100

    New build script which starts dev server to gen PDF, and does PROD build for webpack.

--- scripts/generate-pdf.ts
+++ scripts/generate-pdf.ts
@@ -2,32 +2,42 @@ let cpx = require('cpx');
 let Html5ToPdf = require('html5-to-pdf');
 
 const pdfUrl = 'http://localhost:8000';
-renderHtml(pdfUrl);
-
 const dist = './static/dist/';
 
-function renderHtml(httpUrl: string) {
+// renderHtml(pdfUrl);
 
-    let _htmlToPdf = new Html5ToPdf({
-        templateUrl: httpUrl,
-        inputBody: '',
-        outputPath: 'pdf-render/resume.pdf',
-        renderDelay: 800,
-        template: 'htmlbootstrap',
-        options: {
-            pageSize: 'A4',
-            marginsType: 2,
-            printBackground: true
-        }
+function renderHtml(httpUrl: string) : Promise<void> {
+
+    return new Promise<void>((resolve, reject) => {
+
+        let _htmlToPdf = new Html5ToPdf({
+            templateUrl: httpUrl,
+            inputBody: '',
+            outputPath: 'pdf-render/resume.pdf',
+            renderDelay: 800,
+            template: 'htmlbootstrap',
+            options: {
+                pageSize: 'A4',
+                marginsType: 2,
+                printBackground: true
+            }
+        });
+
+        _htmlToPdf.build(err => {
+            if (err) {
+                console.log(err);
+                throw err;
+            } else {
+                console.log('Generated resume PDF OK. Copying to site.');
+                cpx.copy('./pdf-render/resume.pdf', `${dist}resume/`);
+                resolve();
+            }
+        });
+
     });
 
-    _htmlToPdf.build(err => {
-        if (err) {
-            console.log(err);
-            throw err;
-        } else {
-            console.log('Generated resume PDF OK. Copying to site.');
-            cpx.copy('./pdf-render/resume.pdf', `${dist}resume/`);
-        }
-    });
+}
+
+export default function renderPdf() : Promise<void> {
+    return renderHtml(pdfUrl);
 }
--- static/src/js/static_app.js
+++ static/src/js/static_app.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 import '../scss/site.scss';
 
 if (navigator.userAgent.indexOf('Electron') !== -1) {
-    let htmlNode = document.querySelector('html');
+    var htmlNode = document.querySelector('html');
     htmlNode.classList.add('electron-browser');
 }
+
