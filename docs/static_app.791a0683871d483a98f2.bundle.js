2c0d4376e6f76d0f3aa166e0e75c6542f9447e9a
Owen Berry <owen@owenberry.com>
2023-10-30 17:33:09 +1100

    Simple CSS fixes to start

--- docs/static_app.791a0683871d483a98f2.bundle.js
+++ docs/static_app.791a0683871d483a98f2.bundle.js
@@ -1,2 +1,2 @@
 (()=>{"use strict";-1!==navigator.userAgent.indexOf("PUP_OB")&&document.querySelector("html").classList.add("pup-ob")})();
-//# sourceMappingURL=static_app.320cbbb10116abb82a3d.bundle.js.map
+//# sourceMappingURL=static_app.791a0683871d483a98f2.bundle.js.map
