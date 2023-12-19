a8f5643e9137d7a3ebefd3bb7d05881f704a2ff7
Owen Berry <owen@owenberry.com>
2023-12-19 17:05:43 +1100

    content tweaks for PDF + new build

---
+++ docs/about.b72e0397d2d22f1b7abe.bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";-1!==navigator.userAgent.indexOf("PUP_OB")&&document.querySelector("html").classList.add("pup-ob")})();
+//# sourceMappingURL=about.b72e0397d2d22f1b7abe.bundle.js.map
