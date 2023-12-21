d2ab28e1963ab0f08e3aff2bfc9897a17e86a694
Owen Berry <owen@owenberry.com>
2023-12-21 16:47:09 +1100

    build

---
+++ docs/about.b4cd1d161c7a9903817c.bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";(()=>{function d(d){const i={isPdfBuild:!1};return((d=d??{forcePdfBuild:!1}).forcePdfBuild||-1!==navigator.userAgent.indexOf("PUP_OB"))&&(document.querySelector("html").classList.add("pup-ob"),i.isPdfBuild=!0),i}window._debug=Object.assign({},window._debug,{commonInit:d}),d()})()})();
+//# sourceMappingURL=about.b4cd1d161c7a9903817c.bundle.js.map
--- docs/about.b72e0397d2d22f1b7abe.bundle.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";-1!==navigator.userAgent.indexOf("PUP_OB")&&document.querySelector("html").classList.add("pup-ob")})();
-//# sourceMappingURL=about.b72e0397d2d22f1b7abe.bundle.js.map
