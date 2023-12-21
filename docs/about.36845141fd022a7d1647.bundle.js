cb973cc4bc79c001fbe247843da35c32c5325004
Owen Berry <owen@owenberry.com>
2023-12-22 04:22:28 +1100

    LOTS of small CSS tweaks for PDF & small styles

--- docs/about.36845141fd022a7d1647.bundle.js
+++ docs/about.36845141fd022a7d1647.bundle.js
@@ -1,2 +1,2 @@
 (()=>{"use strict";(()=>{function d(d){const i={isPdfBuild:!1};return((d=d??{forcePdfBuild:!1}).forcePdfBuild||-1!==navigator.userAgent.indexOf("PUP_OB"))&&(document.querySelector("html").classList.add("pup-ob"),i.isPdfBuild=!0),i}window._debug=Object.assign({},window._debug,{commonInit:d}),d()})()})();
-//# sourceMappingURL=about.b4cd1d161c7a9903817c.bundle.js.map
+//# sourceMappingURL=about.36845141fd022a7d1647.bundle.js.map
