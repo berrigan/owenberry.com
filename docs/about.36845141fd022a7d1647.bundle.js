ad2774b37dec50692688bcdc5d0736b4be9988e1
Owen Berry <owen@owenberry.com>
2024-01-02 18:05:33 +1100

    delete docs build

--- docs/about.36845141fd022a7d1647.bundle.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";(()=>{function d(d){const i={isPdfBuild:!1};return((d=d??{forcePdfBuild:!1}).forcePdfBuild||-1!==navigator.userAgent.indexOf("PUP_OB"))&&(document.querySelector("html").classList.add("pup-ob"),i.isPdfBuild=!0),i}window._debug=Object.assign({},window._debug,{commonInit:d}),d()})()})();
-//# sourceMappingURL=about.36845141fd022a7d1647.bundle.js.map
